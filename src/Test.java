d872a9a5f47f6bb68e883356ecd59f1bd819cd46
Michael, Tang Ying Jian <yjtang@gmail.com>
2013-04-15 17:33:14 +0800

    testing git only

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,24 @@
+import michael.findata.service.ReportPubDateService;
+import michael.findata.service.StockPriceService;
+import michael.findata.service.StockService;
 import michael.findata.external.SecurityTimeSeriesData;
 import michael.findata.external.SecurityTimeSeriesDatum;
 import michael.findata.external.tdx.TDXPriceHistory;
-import michael.findata.model.FinDataUpdates;
 import michael.findata.model.Stock;
 import michael.findata.util.ResourceUtil;
+import org.cyberneko.html.parsers.DOMParser;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.XPath;
+import org.dom4j.io.DOMReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.xml.sax.SAXException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.sound.midi.SysexMessage;
 import java.io.IOException;
 import java.sql.*;
 import java.text.ParseException;
@@ -26,9 +36,20 @@ public class Test {
 //		Matcher m = p2.matcher("二十四、(112132、300147) 香雪制药：2012年年度报告主要财务指标及分配预案");
 //		System.out.println(m.find());
 
-		Date dt = new Date();
-		refreshStockPriceHistories();
-		System.out.printf("%d",(new Date().getTime() - dt.getTime()));
+//		Date dt = new Date();
+//		refreshStockPriceHistories();
+//		System.out.printf("%d",(new Date().getTime() - dt.getTime()));
+
+//		ApplicationContext context = new ClassPathXmlApplicationContext("/michael/findata/findata_spring.xml");
+//		ReportPubDateService spds = (ReportPubDateService) context.getBean("reportPubDateService");
+//		StockPriceService sps = (StockPriceService) context.getBean("stockPriceService");
+//		StockService ss = (StockService) context.getBean("stockService");
+//
+//		ss.refreshStockCodes();
+//		sps.refreshStockPriceHistories();
+//		ss.updateFindataWithDates();
+
+        System.out.println("你来了？");
 	}
 
 	// Bulk-load stock pricing data from THS, make sure THS pricing data is complete before doing this!!!!!
@@ -121,5 +142,33 @@ public class Test {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 		Connection con = DriverManager.getConnection(ResourceUtil.getString(JDBC_URL), ResourceUtil.getString(JDBC_USER), ResourceUtil.getString(JDBC_PASS));
 		return con;
+	}
+
+	public static void test() throws IOException, DocumentException, SAXException, SQLException {
+		DOMParser parser = new DOMParser();
+		parser.parse("http://stockdata.stock.hexun.com/2008/lr.aspx?stockid=600016&accountdate=2005.12.31");
+		DOMReader domReader = new DOMReader();
+		Document doc = domReader.read(parser.getDocument());
+		XPath tablePath = doc.createXPath("//DIV[@id=\"zaiyaocontent\"]//TBODY/TR[position()>1 and position()<last()]");
+		XPath fieldNamePath = doc.createXPath(".//STRONG[1]");
+		XPath fieldValuePath = doc.createXPath("./TD[2]/DIV");
+		Element nameElement, valueElement;
+		Object o = tablePath.evaluate(doc);
+
+//		Connection con = DriverManager.getConnection("jdbc.mysql://localhost/findata", "root", "108129");
+//		con.setAutoCommit(false);
+//		Statement stmt = con.createStatement();
+//		stmt.executeQuery("SELECT id, name FROM source ORDER BY id");
+//		stmt.executeQuery("SELECT code FROM stock ORDER BY id");
+//		stmt.executeQuery("SELECT name FROM fin_period ORDER BY id");
+//		stmt.executeQuery("SELECT id, name FROM fin_sheet ORDER BY id");
+
+		for (Element e : (List<Element>) o) {
+			nameElement = (Element) fieldNamePath.evaluate(e);
+			valueElement = (Element) fieldValuePath.evaluate(e);
+			System.out.print(nameElement.getText());
+			System.out.print(" ");
+			System.out.println(valueElement.getText());
+		}
 	}
 }
